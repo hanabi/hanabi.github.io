cc5c31ecfb0856f600b8db57eb6c9cbb33007f10
Zamiel <j.nesta@gmail.com>
2020-06-17 08:09:56 +0000

    better spell check

--- spell_check.sh
+++ spell_check.sh
@@ -3,13 +3,14 @@
 # From: https://github.com/eleven-labs/blog.eleven-labs.com/blob/master/bin/check-spelling.sh
 
 MARKDOWN_TEXT=`(find . -name '*.md' -exec cat {} \;)`
-MISSPELLED=`echo $MARKDOWN_TEXT | aspell --lang=en --encoding=utf-8 list | sort -u`
-# --personal=./.aspell.en.pws list
-NUM_MISSPELLED=`echo "$MISSPELLED" | wc -l`
-if [[ $NB_MISSPELLED -gt 0 ]]; then
-  echo -e "Misspelled words:"
-  MISSPELLED=`echo "$MISSPELLED" | sed -E ':a;N;$!ba;s/\n/, /g'`
-  echo "$MISSPELLED"
-  exit 1
+MISSPELLED=`echo $MARKDOWN_TEXT | aspell --lang=en --encoding=utf-8 --personal=./.aspell.en.pws list | sort -u`
+
+if [[ -z $MISSPELLED ]]; then
+  echo "No misspelled words."
+  exit 0
 fi
-echo "No misspelled words."
+
+echo -e "Misspelled words:"
+MISSPELLED=`echo "$MISSPELLED" | sed -E ':a;N;$!ba;s/\n/, /g'`
+echo "$MISSPELLED"
+exit 1
