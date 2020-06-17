bab2d5c4c4332df11c2cc80d8c3306d08af2b249
Zamiell <j.nesta@gmail.com>
2020-06-17 03:53:56 -0400

    better spell check

--- spell_check.sh
+++ spell_check.sh
@@ -2,10 +2,8 @@
 
 # From: https://github.com/eleven-labs/blog.eleven-labs.com/blob/master/bin/check-spelling.sh
 
-echo "The following markdown files were changed:"
-echo "$TRAVIS_COMMIT_RANGE"
-
 MARKDOWN_TEXT=`(find . -name '*.md' -exec cat {} \;)`
+echo $MARKDOWN_TEXT
 MISSPELLED=`echo $MARKDOWN_TEXT | aspell --lang=en --encoding=utf-8 --personal=./.aspell.en.pws list | sort -u`
 NUM_MISSPELLED=`echo "$MISSPELLED" | wc -l`
 if [[ $NB_MISSPELLED -gt 0 ]]; then
@@ -14,3 +12,4 @@ if [[ $NB_MISSPELLED -gt 0 ]]; then
   echo "$MISSPELLED"
   exit 1
 fi
+echo "No misspelled words."
