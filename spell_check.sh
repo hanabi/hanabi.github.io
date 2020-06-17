8e2663cdea035d2959c16b18da64dc476f291210
Zamiell <j.nesta@gmail.com>
2020-06-17 03:51:28 -0400

    better spell check

--- spell_check.sh
+++ spell_check.sh
@@ -5,14 +5,8 @@
 echo "The following markdown files were changed:"
 echo "$TRAVIS_COMMIT_RANGE"
 
-MARKDOWN_FILES_CHANGED=`(git diff --name-only $TRAVIS_COMMIT_RANGE || true) | grep .md`
-if [[ -z $MARKDOWN_FILES_CHANGED ]]; then
-  echo "No markdown files to check."
-  exit
-fi
-
-TEXT_CONTENT=`cat $(echo "$MARKDOWN_FILES_CHANGED" | sed -E ':a;N;$!ba;s/\n/ /g')`
-MISSPELLED=`echo $TEXT_CONTENT | aspell --lang=en --encoding=utf-8 --personal=./.aspell.en.pws list | sort -u`
+MARKDOWN_TEXT=`(find . -name '*.md' -exec cat {} \;)`
+MISSPELLED=`echo $MARKDOWN_TEXT | aspell --lang=en --encoding=utf-8 --personal=./.aspell.en.pws list | sort -u`
 NUM_MISSPELLED=`echo "$MISSPELLED" | wc -l`
 if [[ $NB_MISSPELLED -gt 0 ]]; then
   echo -e "Misspelled words:"
