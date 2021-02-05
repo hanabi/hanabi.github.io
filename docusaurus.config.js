8c99a8fe60a473e8add62ac458f2d29b97dc4ba0
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-02-05 14:41:44 -0500

    fix for editing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            'https://github.com/Zamiell/hanabi-conventions/edit/main/website/',
+            'https://github.com/Zamiell/hanabi-conventions/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
