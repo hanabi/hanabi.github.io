f0e98b92b3757495f53b9d4cf1c13cffc94460de
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-02-06 01:02:35 -0500

    testing

--- static/js/hotkey.js
+++ static/js/hotkey.js
@@ -1,6 +1,7 @@
 document.onkeydown = function(e) {
   if (e.ctrlKey && e.key == 'k'){
     e.preventDefault();
-    document.getElementById('search_input_react').focus();
+    // We use .click() instead of .focus() because it does not initialize the search properly
+    document.getElementById('search_input_react').click();
   }
 }
