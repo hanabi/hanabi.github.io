92cdb9526f70438f83261073c225c04da45ecc91
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-02-06 01:08:47 -0500

    maybe this will work

--- static/js/hotkey.js
+++ static/js/hotkey.js
@@ -1,10 +1,10 @@
 document.onkeydown = function(e) {
   // Ctrl + K should focus the search bar, emulating the Algolia search UI
   if (e.ctrlKey && e.key == 'k'){
-    // We have to click before focusing because otherwise it does not initialize properly
+    // We have to click after focusing because otherwise it does not initialize properly
     const search = document.getElementById('search_input_react');
-    search.click();
     search.focus();
+    search.click();
 
     // By default, using Ctrl + K in Chrome will open the location bar, so disable this
     e.preventDefault();
