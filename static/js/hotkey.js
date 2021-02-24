bba70d590a9414393283acc5a0857cbb3ce24907
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-02-24 01:21:24 -0500

    better hotkeys

--- static/js/hotkey.js
+++ static/js/hotkey.js
@@ -1,16 +1,18 @@
-// In order for the search box to initialize properly, we have to manually click it
-document.addEventListener('DOMContentLoaded', function() {
-  const search = document.getElementById('search_input_react');
-  search.click();
-});
-
-// Ctrl + K should focus the search bar, emulating the Algolia search UI
 document.onkeydown = function(e) {
-  if (e.ctrlKey && e.key == 'k'){
-    const search = document.getElementById('search_input_react');
-    search.focus();
+  // Debugging
+  // console.log('Key pressed:', e.key);
 
-    // By default, using Ctrl + K in Chrome will open the location bar, so disable this
-    e.preventDefault();
+  if (e.key === 'ArrowLeft') {
+    const buttons = document.getElementsByClassName('pagination-nav__link');
+    if (buttons.length >= 1) {
+      buttons[0].click();
+    }
+  } else if (e.key === 'ArrowRight') {
+    const buttons = document.getElementsByClassName('pagination-nav__link');
+    if (buttons.length >= 2) {
+      buttons[1].click();
+    } else if (buttons.length >= 1) {
+      buttons[0].click();
+    }
   }
 }
