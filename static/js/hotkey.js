4f13d92f8762ec0f926795b45d41c75b1fe265c8
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-02-26 09:58:41 -0500

    enter hotkey

--- static/js/hotkey.js
+++ static/js/hotkey.js
@@ -2,7 +2,12 @@ document.onkeydown = function(e) {
   // Debugging
   // console.log('Key pressed:', e.key);
 
-  if (e.key === 'ArrowLeft') {
+  if (e.key === 'Enter') {
+    const buttons = document.getElementsByClassName('button--secondary button--lg');
+    if (buttons.length >= 1) {
+      buttons[0].click();
+    }
+  } else if (e.key === 'ArrowLeft') {
     const buttons = document.getElementsByClassName('pagination-nav__link');
     if (buttons.length >= 1) {
       buttons[0].click();
