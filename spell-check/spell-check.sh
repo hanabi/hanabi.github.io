934edbed9e504c92a880e2fc7076190c3997b397
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-02-27 16:00:50 -0500

    poop

--- spell-check/spell-check.sh
+++ spell-check/spell-check.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo getting here 1
+
 # Get the directory of this script
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -7,5 +9,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$DIR/get-files.sh"
 
 # Spell check every file using cspell
-cd "$DIR"
+cd "$PARENT_DIR"
 npx cspell --no-summary --config "$PARENT_DIR/.cspell.json" $FILES
+
+echo getting here 2
