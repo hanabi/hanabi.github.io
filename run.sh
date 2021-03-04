f0685284ad48164233bd062511e8e7b1e92736c9
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-03-04 09:33:20 -0500

    better run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,9 @@
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+if [ ! -d "$DIR/image-generator/create-svg-all.py" ]; then
+  "$DIR/build.sh"
+fi
+
 cd "$DIR"
 npm run start
