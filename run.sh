d28e1856786c2c291306da6ce06ba79dc8ae4924
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-03-04 11:15:44 -0500

    bugfix

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ set -e # Exit on any errors
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-if [ ! -d "$DIR/image-generator/create-svg-all.py" ]; then
+if [ ! -d "$DIR/static/img/generated" ]; then
   "$DIR/create-svg-all.sh"
 fi
 
