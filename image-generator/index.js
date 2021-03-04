578a7422986c423eda4042c544f9a0c84b4404ca
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-03-04 12:28:45 -0500

    bugfix for windows

--- image-generator/index.js
+++ image-generator/index.js
@@ -5,15 +5,16 @@ module.exports = function (context, options) {
   return {
     name: "hanabi-docusaurus-plugin",
     configureWebpack(config, isServer, utils) {
+      const createSVGScriptPath = path.resolve(__dirname, "create-svg.sh");
       return {
         module: {
           rules: [
             {
-              test: /image-generator[\/\\]yml\/.*\.yml$/,
+              test: /image-generator.*yml$/,
               use: getFileLoaderUtils().rules.svg().use.concat([{
                 loader: "shell-loader",
                 options: {
-                  script: path.resolve(__dirname, "create-svg.sh"),
+                  script: `bash ${createSVGScriptPath}`,
                 },
               }]),
             },
