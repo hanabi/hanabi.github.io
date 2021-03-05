0ff0cb53a5f34ff2bf88582b433347d814d292a6
Alexey Sokolov <sokolov@google.com>
2021-03-05 09:38:07 +0000

    Use python3 or python (#546)

--- image-generator/create-svg.py
+++ image-generator/create-svg.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
+#!/bin/bash
+# http://stackoverflow.com/questions/18993438/shebang-env-preferred-python-version
+# http://stackoverflow.com/questions/12070516/conditional-shebang-line-for-different-versions-of-python
+""":"
+which python3 >/dev/null 2>&1 && exec python3 "$0" "$@"
+which python  >/dev/null 2>&1 && exec python  "$0" "$@"
+exec echo "Error: requires python"
+":"""
 
 # Imports (standard library)
 import io
