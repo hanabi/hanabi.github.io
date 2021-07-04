62c617bac9c58910c997b1d0a559594a7aa60951
Alexey Sokolov <sokolov@google.com>
2021-07-04 20:48:13 +0100

    Don't install deps from run.sh
    They are installed in install.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,5 @@ set -e # Exit on any errors
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Install Python dependencies
-pip install -r "$DIR/image-generator/requirements.txt" --quiet
-
 cd "$DIR"
 npm run start
