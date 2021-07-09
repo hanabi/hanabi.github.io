de9843038f7ea877944ed7f64dd0d0584e2390b8
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-09 10:42:18 -0400

    fixing spell checker

--- lint.sh
+++ lint.sh
@@ -8,6 +8,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "$DIR"
 
+# Spell check every file using cspell
+npx cspell --no-progress --no-summary "*.md"
+npx cspell --no-progress --no-summary "*.sh"
+npx cspell --no-progress --no-summary "docs/**"
+npx cspell --no-progress --no-summary "image-generator/**"
+npx cspell --no-progress --no-summary "misc/**"
+npx cspell --no-progress --no-summary "src/**"
+npx cspell --no-progress --no-summary "static/js/**"
+
+# Use remark to check Markdown files for errors
 # We set to quiet to output only warnings and errors
 # We set to frail to exit with 1 on warnings (for CI)
 npx remark --quiet --frail docs misc
--- spell-check/get-files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get the directory of this script
-# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PARENT_DIR=$(dirname "$DIR")
-
-# Get the files that we should perform checks on
-FILES=$(find "$PARENT_DIR/docs" "$PARENT_DIR/misc" -type f)
