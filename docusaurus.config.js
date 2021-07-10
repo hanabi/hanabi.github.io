4dd0c1399edfa2dcd9f83af6b00462c850145b51
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-10 16:32:30 -0400

    prettier

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,95 +1,92 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  title: 'The Hyphen-ated Conventions',
-  url: 'https://hanabi.github.io',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'hanabi',
-  projectName: 'hanabi.github.io',
+  title: "The Hyphen-ated Conventions",
+  url: "https://hanabi.github.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "hanabi",
+  projectName: "hanabi.github.io",
   themeConfig: {
     // Vanilla components
     navbar: {
-      title: 'Hyphen-ated Conventions',
+      title: "Hyphen-ated Conventions",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/hyphenated.png',
+        alt: "My Site Logo",
+        src: "img/hyphenated.png",
       },
       items: [
         {
-          to: 'docs/beginner',
-          activeBasePath: 'docs',
-          label: 'Beginner',
-          position: 'left',
+          to: "docs/beginner",
+          activeBasePath: "docs",
+          label: "Beginner",
+          position: "left",
         },
         {
-          to: 'docs/learning-path',
-          activeBasePath: 'docs',
-          label: 'Learning Path',
-          position: 'left',
+          to: "docs/learning-path",
+          activeBasePath: "docs",
+          label: "Learning Path",
+          position: "left",
         },
         {
-          to: 'docs/reference',
-          activeBasePath: 'docs',
-          label: 'Reference',
-          position: 'left',
+          to: "docs/reference",
+          activeBasePath: "docs",
+          label: "Reference",
+          position: "left",
         },
         {
-          to: 'docs/variant-specific',
-          activeBasePath: 'docs',
-          label: 'Variant-Specific',
-          position: 'left',
+          to: "docs/variant-specific",
+          activeBasePath: "docs",
+          label: "Variant-Specific",
+          position: "left",
         },
         {
-          href: 'https://github.com/hanabi/hanabi.github.io/',
-          label: 'Repo',
-          position: 'right',
+          href: "https://github.com/hanabi/hanabi.github.io/",
+          label: "Repo",
+          position: "right",
         },
         {
-          href: 'https://github.com/hanabi/hanabi.github.io/issues',
-          label: 'Proposals',
-          position: 'right',
+          href: "https://github.com/hanabi/hanabi.github.io/issues",
+          label: "Proposals",
+          position: "right",
         },
       ],
     },
 
     // Extra settings
     algolia: {
-      apiKey: 'd78c09bd651631ef68e85a10a605d1e8',
-      indexName: 'hanabi-conventions',
+      apiKey: "d78c09bd651631ef68e85a10a605d1e8",
+      indexName: "hanabi-conventions",
       contextualSearch: false, // Only useful for versioned sites, e.g. docs for v1, v2
     },
     colorMode: {
-      defaultMode: 'dark',
+      defaultMode: "dark",
     },
     sidebarCollapsible: true,
     hideableSidebar: true,
   },
-  plugins: [
-    path.resolve(__dirname, 'image-generator'),
-  ],
+  plugins: [path.resolve(__dirname, "image-generator")],
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/hanabi/hanabi.github.io/edit/main/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/hanabi/hanabi.github.io/edit/main/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
   ],
   scripts: [
     {
-      src: 'https://kit.fontawesome.com/1932a73877.js',
-      crossorigin: 'anonymous',
+      src: "https://kit.fontawesome.com/1932a73877.js",
+      crossorigin: "anonymous",
     },
-    '/js/hotkey.js',
+    "/js/hotkey.js",
   ],
 };
