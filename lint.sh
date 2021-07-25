b24b9f1fa6042c67746cfae3391cc2e34f3d46de
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-24 20:57:42 -0400

    better lint script

--- lint.sh
+++ lint.sh
@@ -15,11 +15,19 @@ fi
 
 cd "$DIR"
 
-# Use ESLint to lint the JavaScript
-npx eslint src
-npx eslint static/js
+# Step 1 - Use ESLint to lint the JavaScript
+# Since all ESLint errors are set to warnings,
+# we set max warnings to 0 so that warnings will fail in CI
+npx eslint --max-warnings 0 src
+npx eslint --max-warnings 0 static/js
 
-# Spell check every file using cspell
+# Step 2 - Use remark to check Markdown files for errors
+# We set to quiet to output only warnings and errors
+# We set to frail to exit with 1 on warnings (for CI)
+npx remark --quiet --frail docs misc
+
+# Step 3 - Spell check every file using cspell
+# We use no-progress and no-summary because we want to only output errors
 npx cspell --no-progress --no-summary "*.md"
 npx cspell --no-progress --no-summary "*.sh"
 npx cspell --no-progress --no-summary "docs/**"
@@ -27,10 +35,5 @@ npx cspell --no-progress --no-summary "image-generator/**"
 npx cspell --no-progress --no-summary "misc/**"
 npx cspell --no-progress --no-summary "src/**"
 npx cspell --no-progress --no-summary "static/js/**"
-
-# Use remark to check Markdown files for errors
-# We set to quiet to output only warnings and errors
-# We set to frail to exit with 1 on warnings (for CI)
-npx remark --quiet --frail docs misc
 
 echo "Success!"
