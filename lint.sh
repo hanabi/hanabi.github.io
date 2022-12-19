69e17a0c2690e6b4c5c472140556d7ac4185a794
Evan Chen <evan@evanchen.cc>
2022-12-19 13:25:30 -0500

    Fix step numbers in lint.sh (#1050)
    * Fix step numbers in lint.sh
    
    * Update lint.sh
    
    Co-authored-by: James <5511220+Zamiell@users.noreply.github.com>

--- lint.sh
+++ lint.sh
@@ -17,19 +17,19 @@ npx prettier --check .
 npx eslint --max-warnings 0 src
 npx eslint --max-warnings 0 static/js
 
-# Step 2 - Use remark to check Markdown files for errors.
+# Step 3 - Use remark to check Markdown files for errors.
 # We set to quiet to output only warnings and errors.
 # We set to frail to exit with 1 on warnings (for CI).
 npx remark --quiet --frail docs misc
 
-# Step 3 - Spell check every file using cspell.
+# Step 4 - Spell check every file using cspell.
 # We use no-progress and no-summary because we want to only output errors.
 npx cspell --no-progress --no-summary
 
-# Step 4 - Check for orphaned words.
+# Step 5 - Check for orphaned words.
 bash "$DIR/check-orphaned-words.sh"
 
-# Step 5 - Check for unused YML files
+# Step 6 - Check for unused YML files
 python "$DIR/image-generator/check_unused.py"
 
 echo "Success!"
