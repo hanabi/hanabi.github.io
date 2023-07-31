0e03da53d8d03b11a1d5f737ac00360d9bfb95c6
James <5511220+Zamiell@users.noreply.github.com>
2023-07-31 10:12:26 -0400

    fix: lint

--- lint.sh
+++ lint.sh
@@ -8,27 +8,27 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "$DIR"
 
-# Step 1 - Use Prettier to check formatting.
-npx prettier --check .
+# Use Prettier to check formatting.
+# "--log-level=warn" makes it only output errors.
+npx prettier --loglevel=warn --check .
 
-# Step 2 - Use ESLint to lint the JavaScript
-# Since all ESLint errors are set to warnings, we set max warnings to 0 so that warnings will fail
-# in CI.
+# Use ESLint to lint the JavaScript.
+# "--max-warnings 0" makes warnings fail in CI, since we set all ESLint errors to warnings.
 npx eslint --max-warnings 0 .
 
-# Step 3 - Use remark to check Markdown files for errors.
-# We set to quiet to output only warnings and errors.
-# We set to frail to exit with 1 on warnings (for CI).
+# Use remark to check Markdown files for errors.
+# "--quiet" output only warnings and errors.
+# "--frail" exits with 1 on warnings (for CI).
 npx remark --quiet --frail docs misc
 
-# Step 4 - Spell check every file using cspell.
-# We use no-progress and no-summary because we want to only output errors.
-npx cspell --no-progress --no-summary
+# Spell check every file using CSpell.
+# "--no-progress" and "--no-summary" make it only output errors.
+npx cspell --no-progress --no-summary .
 
-# Step 5 - Check for orphaned words.
-bash "$DIR/check-orphaned-words.sh"
+# Check for unused CSpell words.
+npx cspell-check-unused-words
 
-# Step 6 - Check for unused YML files
+# Check for unused YML files.
 python "$DIR/image-generator/check_unused.py"
 
 echo "Success!"
