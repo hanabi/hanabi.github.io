542dbd35c18f539a3fd9640b2a4362a39a24c85b
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-01-14 12:28:27 -0500

    chore: fix check_valid script

--- image-generator/check_valid.sh
+++ image-generator/check_valid.sh
@@ -10,13 +10,18 @@ SECONDS=0
 
 cd "$DIR"
 
+PYTHON_COMMAND="python"
+if command -v "python3" > /dev/null; then
+  PYTHON_COMMAND="python3"
+fi
+
 # Test to see if all of the YAML files are valid by manually invoking the "create_svg.py" script on
 # every YAML file.
 # echo "Testing to see if all of the YAML files are valid..."
 YAML_FILES=$(find "$DIR/yml" -name '*.yml' -type f)
 for YAML_FILE in $YAML_FILES; do
   # echo "$YAML_FILE"
-  python "$DIR/create_svg.py" < "$YAML_FILE" > /dev/null
+  $PYTHON_COMMAND "$DIR/create_svg.py" < "$YAML_FILE" > /dev/null
 done
 
 # echo "All the YAML files are valid. (It took $SECONDS seconds.)"
