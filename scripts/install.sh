a4be2d4a062201e5fc5334931e31f8ab654cdac2
Alexey Sokolov <alexey@asokolov.org>
2024-10-02 23:27:59 +0100

    Don't install python dep

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,30 +15,9 @@ if [[ $OSTYPE == 'darwin'* ]]; then
   if ! which -s node; then
     brew install node
   fi
-
-  if ! which -s python3; then
-    brew install python3
-  fi
 fi
 
 echo "Installing JavaScript dependencies..."
 cd "$DIR"
 npm ci
-echo "Complete."
-
-echo "Installing Python dependencies..."
-cd "$DIR/../image-generator"
-if command -v "pip3" > /dev/null; then
-  pip3 install -r requirements.txt
-elif command -v "pip" > /dev/null; then
-  # Check to make sure that pip is for Python 3, not Python 2
-  if [[ ! $(pip3 --version | sed -n 's/^.*(python \(.\).*/\1/p' | grep 3) ]]; then
-    echo "Error: Your \"pip\" command appears to be associated with Python 2. Python 3 is required for this website."
-    exit 1
-  fi
-  pip install -r requirements.txt
-else
-  echo "Error: You do not appear to have pip installed, which is required for this website."
-  exit 1
-fi
 echo "Complete."
