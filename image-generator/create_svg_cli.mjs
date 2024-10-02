aacf0243a6b0602e933d3d7a817bcde945509abf
Alexey Sokolov <alexey@asokolov.org>
2024-10-02 23:42:23 +0100

    Put the CLI back

---
+++ image-generator/create_svg_cli.mjs
@@ -0,0 +1,20 @@
+// Does the same thing as the loader, but without webpack.
+// Useful for manual testing, and also for linter.
+
+import create_svg from "./create_svg.cjs";
+
+const chunks = [];
+const readable = process.stdin;
+
+readable.on("readable", () => {
+  let chunk;
+  while (null !== (chunk = readable.read())) {
+    chunks.push(chunk);
+  }
+});
+
+readable.on("end", () => {
+  const content = chunks.join("");
+  const data = create_svg(content);
+  process.stdout.write(data);
+});
