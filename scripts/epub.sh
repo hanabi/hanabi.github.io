c98bb295aa5c46ad709c1c0eecbd6b1cc42c6381
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-11-23 00:26:27 -0500

    fix prettier

--- scripts/epub.sh
+++ scripts/epub.sh
@@ -7,9 +7,9 @@ epubsrc="build/assets/epub"
 
 mkdir -p $epubsrc
 cp -r static/epub/epub-template/ "$epubsrc/epub-src/"
-cp    static/img/cover.png       "$epubsrc/epub-src/OEBPS/img"
-cp -r static/img/pieces          "$epubsrc/epub-src/OEBPS/img"
-cp -r static/img/examples        "$epubsrc/epub-src/OEBPS/img"
+cp static/img/cover.png "$epubsrc/epub-src/OEBPS/img"
+cp -r static/img/pieces "$epubsrc/epub-src/OEBPS/img"
+cp -r static/img/examples "$epubsrc/epub-src/OEBPS/img"
 cd $epubsrc
 
 python3 ../../../scripts/epub.py
@@ -18,9 +18,8 @@ filename="hgroup-conventions.epub"
 epubout="epub-src/out/"
 mkdir -p $epubout
 cd epub-src
-zip -X0   "../$epubout$filename" mimetype
+zip -X0 "../$epubout$filename" mimetype
 zip -9 -r "../$epubout$filename" META-INF/ OEBPS/ -x '*.DS_Store'
 cd ..
 mv "$epubout$filename" "$filename"
 rm -rf epub-src
-
