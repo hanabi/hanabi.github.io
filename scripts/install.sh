68e7684b3cd5bc2dd84f4f961f89bf841d6b7de1
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-11-25 09:43:30 -0500

    chore: simplify installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,15 +6,9 @@ set -euo pipefail # Exit on errors and undefined variables.
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-if [[ "$(uname)" == "Darwin" ]]; then
-  # Install Homebrew if it already doesn't exist.
-  if ! command -v brew &> /dev/null; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  fi
-
-  if ! command -v node &> /dev/null; then
-    brew install node
-  fi
+if ! command -v npm &> /dev/null; then
+  echo "Error: The \"npm\" command was not found. You must first install Node.js (and have \"npm\" in your PATH) before running this installation script." >&2
+  exit 1
 fi
 
 echo "Installing JavaScript dependencies..."
