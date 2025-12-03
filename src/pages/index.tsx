44fa9e4a94dda8bbb94e42c623f4f20233b0e0a1
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-12-02 21:47:36 -0500

    fix: lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,10 +40,7 @@ function HomepageHeader() {
 
 function Home(): React.JSX.Element {
   return (
-    <Layout
-      title="Home"
-      description="Strategies for Hanabi, a cooperative card game of logic and reasoning"
-    >
+    <Layout>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
