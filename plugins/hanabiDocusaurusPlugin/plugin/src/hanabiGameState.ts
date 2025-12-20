8ae901956fd4820e3f7488489d118998eb2124f9
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-12-20 11:04:34 -0500

    update

--- plugins/hanabiDocusaurusPlugin/plugin/src/hanabiGameState.ts
+++ plugins/hanabiDocusaurusPlugin/plugin/src/hanabiGameState.ts
@@ -73,7 +73,10 @@ const card = z
      */
     above: z.string().min(1).or(textObject).optional(),
 
-    /** A text label in the middle of the card. (This string must be short to fit on the card.) */
+    /**
+     * A text label in the middle of the card. (If the label is too long, it would wrap to a second
+     * line and look buggy, so we limit the amount of characters.)
+     */
     middleNote: z.coerce.string().min(1).max(6).optional(),
 
     /** A text label below the card. Can be either a string or an object representing a string. */
